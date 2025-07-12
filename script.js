89c8230860fb3a619cf9980ca3ac8bc48e6b039e
Mutasim Abdalla Esmaiel Kharif <111911372+MutasimKharif@users.noreply.github.com>
2025-07-12 03:36:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () =>
     {
-    const pageName = window.location.pathname.split('/').pop().replace('.html', '') || 'index';
+    const pageName = window.location.'index.html';
 // --- TEMPLATE GENERATORS ---
     const createHeaderHTML = (activePage) => {
         const pages = [
